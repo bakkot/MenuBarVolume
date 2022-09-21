0579aac43f18628c5bf269f7b7b2cdaa3881ced9
Kevin Gibbons <bakkot@gmail.com>
2022-09-21 12:06:53 -0700

    Initial Commit

---
+++ MenuBarVolume/MenuBarVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarVolumeApp.swift
+//  MenuBarVolume
+//
+//  Created by Kevin on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
